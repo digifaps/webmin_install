f20bb65b818aad10b7ef208bd7c2f028f83b3866
digifaps <fabricezander@gmail.com>
2016-03-22 20:26:17 +0100

    made it much ore intelligent and verbose

--- webmin_install_debian.sh
+++ webmin_install_debian.sh
@@ -1,4 +1,20 @@
-sudo sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
-wget -qO - http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install webmin
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo ':: gettings key and adding repository to the sources'
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+wget -O- http://www.webmin.com/jcameron-key.asc | apt-key add -
+
+echo ':: updatings lists'
+apt-get update
+
+echo ':: installing ...'
+apt-get install webmin
+
+echo ':: done !'
+echo '--------------------------'
+echo 'go to https://<adress>:10000 in a browser and login with a user account that has sudo rights'
+
